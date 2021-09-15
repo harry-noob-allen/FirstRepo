42a0b7598013f84afe2f176e47953c13a6445928
kumar <kumar@192.168.1.3>
2021-09-15 12:14:35 +0530

    Added booking

---
+++ LTIAirlines/src/main/java/com/lti/model/Booking.java
@@ -0,0 +1,21 @@
+package com.lti.model;
+
+public class Booking {
+	
+	int bookingId;
+
+	public Booking(int bookingId) {
+		super();
+		this.bookingId = bookingId;
+	}
+
+	public int getBookingId() {
+		return bookingId;
+	}
+
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+	
+
+}
